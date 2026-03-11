c6dbd4102c1ff4929c0ec9e04df8e29358003957
Thiago Ribeiro <thiagitosouza@gmail.com>
2026-03-11 12:42:58 -0300

    add markdow preview

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = "cd app && npm install",
+    keys = {
+      { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
+    },
+  },
+}
